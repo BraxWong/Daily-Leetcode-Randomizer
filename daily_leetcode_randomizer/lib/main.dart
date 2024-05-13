9749b15828585e13ead9c9a9fd12e9a782e04b18
Brax Wong <ghostenjob@gmail.com>
2024-05-13 20:54:39 +0100

    feature: added a stateful textfield to allow users to input the question that they want to do.

--- daily_leetcode_randomizer/lib/main.dart
+++ daily_leetcode_randomizer/lib/main.dart
@@ -7,27 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -40,7 +24,72 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text('Daily LeetCode Randomizer'))
+      appBar: AppBar(
+        title: Text('Daily LeetCode Randomizer')
+      ),
+      body: Column(
+        children: <Widget>[AppSlogan(), SearchLeetCodeQuestion()]
+      ) 
+    );
+  }
+}
+
+class AppSlogan extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Text('One LeetCode question a day and ace all of your technical interviews');
+  }
+}
+
+//Taking constructor arguments and setting up override
+class SearchLeetCodeQuestion extends StatefulWidget {
+  @override
+  _SearchLeetCodeQuestionState createState() => _SearchLeetCodeQuestionState();
+}
+
+/*
+ * Handling states and rendering
+ */
+class _SearchLeetCodeQuestionState extends State<SearchLeetCodeQuestion> {
+  /*
+   * Used to get the text within the TextField widget
+   */
+  final controller = TextEditingController();
+  String question = "";
+  /*
+   *when this widget is done, need to clean up everything. Kind of like a destructor.
+   */
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void setQuestion(question){
+    /*
+     * setState is a flutter method that forces flutter to reload
+     */
+    setState((){
+      this.question = question; 
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: this.controller,
+          decoration: InputDecoration(
+            prefixIcon: Icon(
+              Icons.message
+            ),
+            labelText: "Enter the name of a question that you want to practice."
+          ),
+          onChanged: (text) => this.setQuestion(text), 
+        ),
+        Text(this.question)
+      ]
     );
   }
 }
