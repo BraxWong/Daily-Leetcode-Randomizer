7148ad92e05aca58e53c91a51bd33aaa2c901527
Brax Wong <ghostenjob@gmail.com>
2024-05-23 19:09:18 +0100

    refactor: fixed the mess caused by firebase.

--- daily_leetcode_randomizer/lib/Login.dart
+++ daily_leetcode_randomizer/lib/Login.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 import 'HomePage.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
-Future<void> main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
 class Login extends StatelessWidget {
   @override
